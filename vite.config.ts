28c1f0cc390bd2a256812f869c9ee08598af21e2
Adishwar Rishi <adiswa123@gmail.com>
2022-10-21 19:59:44 +1100

    Setup eslint and module bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import legacy from '@vitejs/plugin-legacy';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy({ targets: ['defaults'] })],
+  build: { sourcemap: true },
+  plugins: [
+    react(),
+    legacy({ targets: ['defaults'] }),
+    visualizer({ emitFile: true, gzipSize: true }),
+  ],
 });
