1adc1a19af6ad1b6c80dbc6e7ebc6d85e7d4a1d8
Adishwar Rishi <adiswa123@gmail.com>
2023-03-17 15:09:52 +1100

    Improve setupTests with an explicit extend for matchers

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,8 @@
  * Provides provides custom DOM element matchers for expect()
  * https://testing-library.com/docs/ecosystem-jest-dom
  */
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom'; // tells typescript about additional expect() matchers
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+expect.extend(matchers);
