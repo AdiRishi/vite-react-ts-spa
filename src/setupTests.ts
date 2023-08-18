d07f30d80b8ae33d6cb0df7ef5be5ce3c82f1342
Adishwar Rishi <adiswa123@gmail.com>
2023-08-18 13:28:46 +1000

    Upgrade testing library jest-dom + eslint to v6

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,7 +2,7 @@
  * Provides provides custom DOM element matchers for expect()
  * https://testing-library.com/docs/ecosystem-jest-dom
  */
-import '@testing-library/jest-dom'; // tells typescript about additional expect() matchers
+import '@testing-library/jest-dom/vitest'; // tells typescript about additional expect() matchers
 import matchers from '@testing-library/jest-dom/matchers';
 import { expect } from 'vitest';
 
